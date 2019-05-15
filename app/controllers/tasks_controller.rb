a06180742026d2b63bbe7ce2487978808ed3a593
t.y <tack_s1123@outlook.jp>
2019-05-15 13:56:03 +0000

    Third

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -47,10 +47,10 @@ class TasksController < ApplicationController
     redirect_to tasks_url
     
   end
-end
 
-private
-
-def task_params
-  params.require(:task).permit(:content)
+  private
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
 end
